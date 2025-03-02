b65754b9cee4e126b02c91be407664408a51113f
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2025-03-03 00:09:51 +0900

    [add] - Tier 객체 추가 - Tier 이미지파일 추가

--- src/common/Tier.ts
+++ src/common/Tier.ts
@@ -1,12 +1,35 @@
-export const Tier: { [key: number]: string } = Object.freeze({
-    10: 'Iron',
-    9: 'Bronze',
-    8: 'Silver',
-    7: 'Gold',
-    6: 'Platinum',
-    5: 'Emerald',
-    4: 'Diamond',
-    3: 'Master',
-    2: 'Grandmaster',
-    1: 'Challenger',
-});
+class Tier {
+    getTierName(tier: number): string {
+        switch (tier) {
+            case 10: return 'Iron';
+            case 9: return 'Bronze';
+            case 8: return 'Silver';
+            case 7: return 'Gold';
+            case 6: return 'Platinum';
+            case 5: return 'Emerald';
+            case 4: return 'Diamond';
+            case 3: return 'Master';
+            case 2: return 'Grandmaster';
+            case 1: return 'Challenger';
+            default: return 'Unknown Tier';
+        }
+    }
+
+    getTierImage(tier: number): string {
+        switch (tier) {
+            case 10: return '/assets/tier/Iron.png';
+            case 9: return '/assets/tier/bronze.png';
+            case 8: return '/assets/tier/silver.png';
+            case 7: return '/assets/tier/gold.png';
+            case 6: return '/assets/tier/platinum.png';
+            case 5: return '/assets/tier/emerald.png';
+            case 4: return '/assets/tier/diamond.png';
+            case 3: return '/assets/tier/master.png';
+            case 2: return '/assets/tier/grandmaster.png';
+            case 1: return '/assets/tier/challenger.png';
+            default: return '/assets/bronze.png';
+        }
+    }
+}
+
+export default Tier;
--- src/page/mainlayout/DummyPage2.tsx
+++ src/page/mainlayout/DummyPage2.tsx
@@ -1,58 +1,99 @@
-import {useGroupList, useGroupsByGroupId} from "@/api/group/group";
-import {Tier} from "@/common/Tier";
+import { useFetchGroupList, useFetchGroupsUserByGroupId } from "@/api/group/group";
 
+import { useEffect, useRef, useState, useCallback } from "react";
+import { tftUserInfo } from "@/api/user/model.tft";
+import Tier from "@/common/Tier";
+
+/**
+ * 최적화 필요. 불필요한 렌더링이 한번 일어나는것 같음
+ * @constructor
+ */
 
 const DummyPage2 = () => {
-    const { data: groupData, isLoading: isGroupLoading } = useGroupList();
+    const tier = new Tier()
+    const { data: groupData, isLoading: isGroupLoading, isSuccess } = useFetchGroupList();
     const firstGroupId = groupData?.groups?.[0]?.id;
-    const { data: groupUserList, isLoading: isUserLoading } = useGroupsByGroupId(firstGroupId);
 
-    if(isGroupLoading) {
-        return <div> 내가속한 그룹가져오는중..</div>
+    const [page, setPage] = useState<number>(0);
+    const [hasNext, setHasNext] = useState<boolean>(false);
+
+    const { data: groupUserResponse, isLoading: isUserLoading } = useFetchGroupsUserByGroupId(firstGroupId, page);
+    const [groupUserList, setGroupUserList] = useState<tftUserInfo[]>([]);
+
+    const observerRef = useRef<IntersectionObserver | null>(null);
+    const observerTargetRef = useRef<HTMLDivElement | null>(null);
+
+
+    const handleObserver = useCallback((entries: IntersectionObserverEntry[]) => {
+        console.log("현재 hasNext1",hasNext);
+        const target = entries[0];
+        if (target.isIntersecting && !isUserLoading && hasNext) {
+            setPage((prevPage) => prevPage + 1);
+        }
+    }, [hasNext]);
+
+
+    useEffect(() => {
+        console.log("현재 hasNext2",hasNext);
+        if (observerRef.current) observerRef.current.disconnect();
+
+        observerRef.current = new IntersectionObserver(handleObserver, { threshold: 0 });
+
+        if (observerTargetRef.current) {
+            observerRef.current.observe(observerTargetRef.current);
+        }
+
+        return () => observerRef.current?.disconnect();
+    }, [handleObserver]);
+
+
+    useEffect(() => {
+        console.log("현재 hasNext3",hasNext);
+        if (groupUserResponse) {
+            setGroupUserList((prevList) => [...prevList, ...groupUserResponse.data]);
+
+            setHasNext(groupUserResponse.hasNext);
+        }
+    }, [groupUserResponse]);
+
+    if (isGroupLoading) {
+        console.log("내가 속한 그룹 리스트 조회 중...");
+    }
+    if (isSuccess) {
+        console.log("내가 속한 그룹 리스트 조회 끝:", firstGroupId);
     }
 
     if (!firstGroupId) {
+        console.log("내가 속한 그룹 없음");
         return <div>속한 그룹이 없습니다. 그룹 가입 페이지로 이동하세요.</div>;
     }
 
-    // useEffect(() => {
-    //     if (!isGroupLoading && !firstGroupId) {
-    //         navigate("dummy3")
-    //     }
-    // }, [isGroupLoading, firstGroupId]);
-
-
-
     return (
         <div>
-            <h1>내가속한 그룹중 제일처음거는 {groupData?.groups[0].name}</h1>
-            {
-                isUserLoading ? (
-                    <div>그룹내부 유저들 가져오는중</div>
-                ) : (
-                    <ul>
-                        {
-                            groupUserList?.data.map((user,index) => (
-                                <li key={user.summoner.id}>
-                                    <div>
-                                        <p>{user.user.nickname}</p>
-                                        <img
-                                            src={`${user.user.profileImageUrl}`}
-                                            alt="프로필 이미지"/>
-                                        <p>{index+1}등입</p>
-                                        <img
-                                            src={`https://ddragon.leagueoflegends.com/cdn/15.1.1/img/profileicon/${user.summoner.profileIconId}.png`}
-                                            alt="프로필아이콘"/>
-                                        <p>{user.summoner.gameName} {user.summoner.tagLine}</p>
-                                        <p>{Tier[user.summoner.entry.RANKED_TFT.tier]}</p>
-                                        <p>{user.summoner.entry.RANKED_TFT.rank}</p>
-                                    </div>
-                                </li>)
-                            )
-                        }
-                    </ul>
-                )
-            }
+            <h1>내가 속한 그룹 중 첫 번째 그룹: {groupData?.groups[0].name}</h1>
+            <ul>
+                {groupUserList.map((user, index) => (
+                    <li key={user.summoner.id}>
+                        <div>
+                            <p>{user.user.nickname}</p>
+                            <img src={`${user.user.profileImageUrl}`} alt="프로필 이미지"/>
+                            <p>{index + 1}등</p>
+                            <img
+                                src={`https://ddragon.leagueoflegends.com/cdn/15.1.1/img/profileicon/${user.summoner.profileIconId}.png`}
+                                alt="프로필 아이콘"
+                            />
+                            <p>
+                                {user.summoner.gameName} {user.summoner.tagLine}
+                            </p>
+                            <p>{tier.getTierName(user.summoner.entry.RANKED_TFT.tier)}</p>
+                            <img src={`${tier.getTierImage(user.summoner.entry.RANKED_TFT.tier)}`}/>
+                            <p>{user.summoner.entry.RANKED_TFT.rank}</p>
+                        </div>
+                    </li>
+                ))}
+                {isUserLoading && <div>그룹 내부 유저들 가져오는 중...</div>}
+                <div ref={observerTargetRef} style={{ height: "10px" }}></div>
+            </ul>
         </div>
     );
 };
