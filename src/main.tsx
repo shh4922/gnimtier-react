3fa83e8ea5cd8aaa607177217ba31185fac3a180
Sin Hyeon Ho <83321146+shh4922@users.noreply.github.com>
2025-03-03 00:12:17 +0900

    [add] - query-devtool 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from "react-router-dom";
 import App from "@/App";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
           <BrowserRouter>
               <App/>
           </BrowserRouter>
+          <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
   </StrictMode>,
 )
